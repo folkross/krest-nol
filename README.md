11ecf07053e57d0a49d4982e8dd72caa1c354e1d
folkross <64282651+folkross@users.noreply.github.com>
2023-11-17 16:16:25 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# krest-nol
-krest-nol
+# Tic-Tac-Toe
+A beginner Python project which allows you to play the classic game of Tic-Tac-Toe viz the comfort of your terminal.
+
+### Running Tic-Tac-Toe on your Desktop:
+```
+git clone "https://github.com/nisarg42/Tic-Tac-Toe"
+Open the terminal/cmd within cloned directory
+Enter the command: python run.py
+```
+
+### Features of Tic-Tac-Toe:
+**mini-max Algorithm:** Tic-Tac-Toe has the "computer" coded using the AI algorithm known as mini-max which ensures that the "computer" will never lose.
---
+++ game/helper_functions.py
@@ -0,0 +1,85 @@
+def grid(side=3):
+    # Создание сетки игры
+    game_grid = [[' ' for i in range(side)] for j in range(side)]
+
+    return game_grid
+
+
+def grid_numbering(game_grid):
+    # Нумерация клеток на сетке игры
+    number_dict = {
+
+    }
+
+    location_dict = {
+
+    }
+
+    counter = 0
+    for i in range(len(game_grid)):
+        for j in range(len(game_grid[i])):
+            counter += 1
+            number_dict[counter] = (i, j)
+            location_dict[(i, j)] = counter
+
+    return number_dict, location_dict
+
+
+def check_winner(game_grid):
+    victory, winner = False, None
+
+    columns = []
+
+    for i in range(len(game_grid)):
+        columns.append([game_grid[j][i] for j in range(len(game_grid))])
+    # инициализация диагоналей
+    diagonal_one, diagonal_two = [], []
+    for i in range(len(game_grid)):
+        for j in range(len(game_grid)):
+            if(i == j):
+                diagonal_one.append(game_grid[i][j])
+            if(i+j == len(game_grid)-1):
+                diagonal_two.append(game_grid[i][j])
+    # Проверка горизонтальных линий
+    for row in game_grid:
+        if(all((x == row[0] and x != ' ') for x in row)):
+            victory = True
+            if(row[0] == 'X'):
+                winner = 'X'
+            elif(row[0] == '0'):
+                winner = '0'
+    # Проверка вертикальных линий
+    for column in columns:
+        if(all((x == column[0] and x != ' ') for x in column)):
+            victory = True
+            if(column[0] == 'X'):
+                winner = 'X'
+            elif(column[0] == '0'):
+                winner = '0'
+    # Проверка диагоналей
+    if(all((x == diagonal_one[0] and x != ' ') for x in diagonal_one)):
+        victory = True
+        if(diagonal_one[0] == 'X'):
+            winner = 'X'
+        elif(diagonal_one[0] == '0'):
+            winner = '0'
+    if(all((x == diagonal_two[0] and x != ' ') for x in diagonal_two)):
+        victory = True
+        if(diagonal_two[0] == 'X'):
+            winner = 'X'
+        elif(diagonal_two[0] == '0'):
+            winner = '0'
+
+    counter = 0
+    # Проверка наличия незаполненных ячеек в сетке игры
+    for i in range(len(game_grid)):
+        for j in range(len(game_grid[i])):
+            if(game_grid[i][j] == ' '):
+                counter += 1
+
+    if(counter == 0 and winner == None):
+        winner = 'Tie'
+
+    return [victory, winner]
+
+
